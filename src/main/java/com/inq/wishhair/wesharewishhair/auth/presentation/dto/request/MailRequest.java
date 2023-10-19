c048b5fcd1ab834184558e9eb9ce1bdb18f52157
EunChanNam <namhm23@gmail.com>
2023-10-19 19:35:02 +0900

    refactor: 메일 인증방식 세션에서 DB 를 통한 인증으로 교체

--- src/main/java/com/inq/wishhair/wesharewishhair/auth/presentation/dto/request/MailRequest.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/presentation/dto/request/MailRequest.java
@@ -1,16 +1,9 @@
 package com.inq.wishhair.wesharewishhair.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class MailRequest {
-
+public record MailRequest(
 	@NotNull
-	private String email;
+	String email
+) {
 }
