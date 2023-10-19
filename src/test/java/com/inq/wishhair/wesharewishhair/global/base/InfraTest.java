b9fcfe9ba681d2cc3cc5c76c791a33d8a8112112
EunChanNam <namhm23@gmail.com>
2023-10-19 13:38:50 +0900

    기존 테스트 코드 삭제

--- src/test/java/com/inq/wishhair/wesharewishhair/global/base/InfraTest.java
+++
@@ -1,8 +0,0 @@
-package com.inq.wishhair.wesharewishhair.global.base;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public abstract class InfraTest {
-}
