a7e22587f2da0328ed4b46a1d397527eafbaf986
EunChanNam <namhm23@gmail.com>
2023-10-27 19:51:54 +0900

    test: Auth Api 테스트

--- src/main/java/com/inq/wishhair/wesharewishhair/global/config/SecurityConfig.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/global/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.inq.wishhair.wesharewishhair.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,10 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.csrf(AbstractHttpConfigurer::disable).build();
 	}
 }
