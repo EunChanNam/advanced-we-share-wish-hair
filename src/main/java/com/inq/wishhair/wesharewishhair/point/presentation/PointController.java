bc700263a7a39692950ff123c8d42ba34f3ed249
EunChanNam <namhm23@gmail.com>
2023-11-04 16:22:46 +0900

    test: 포인트 조회 API 테스트

--- src/main/java/com/inq/wishhair/wesharewishhair/point/presentation/PointController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/point/presentation/PointController.java
@@ -1,14 +1,20 @@
 package com.inq.wishhair.wesharewishhair.point.presentation;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inq.wishhair.wesharewishhair.global.annotation.FetchAuthInfo;
+import com.inq.wishhair.wesharewishhair.global.dto.response.PagedResponse;
 import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
 import com.inq.wishhair.wesharewishhair.global.resolver.dto.AuthInfo;
+import com.inq.wishhair.wesharewishhair.point.application.PointSearchService;
+import com.inq.wishhair.wesharewishhair.point.application.dto.PointResponse;
 import com.inq.wishhair.wesharewishhair.point.application.dto.PointUseRequest;
 import com.inq.wishhair.wesharewishhair.point.application.PointService;
 
@@ -29,5 +35,16 @@ public class PointController {
 		pointService.usePoint(request, authInfo.userId());
 
 		return ResponseEntity.ok(new Success());
+	}
+
+	private final PointSearchService pointSearchService;
+
+	@GetMapping
+	public ResponseEntity<PagedResponse<PointResponse>> findPointHistories(
+		final @FetchAuthInfo AuthInfo authInfo,
+		final @PageableDefault Pageable pageable
+	) {
+
+		return ResponseEntity.ok(pointSearchService.getPointHistories(authInfo.userId(), pageable));
 	}
 }
