72433e0f883d3d77782f34f680d52b7cd6f8171e
EunChanNam <namhm23@gmail.com>
2023-11-15 20:27:12 +0900

    chore: cors 에 서버 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/global/config/WebConfig.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/global/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8080")
+			.allowedOrigins("http://localhost:8080", "http://13.59.136.85:8080")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*");
 	}
